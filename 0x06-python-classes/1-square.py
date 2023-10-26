e60ce6d7560427c7dc005703d4615e7b178110ad
Tommy-Dan <ttuckerwra@gmail.com>
2023-10-26 07:25:49 -0700

    class Square that defines a square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
+
 """class Square that defines a square"""
+
 
 class Square:
     """class Square that defines a square"""
     def __init__(self, size):
-        """the initialization function for the square class.
-           Args:
-              size(int): the size of the new square.
-        """
+
+    """the initialization function for the square class.
+        Args:
+        size(int): the size of the new square.
+    """
         self.__size = size
